1513bd3d865f84eac4b6af1d5588f189af80821b
Jaswant <jaswantkasinedi@gmail.com>
2024-09-25 18:40:34 -0700

    Create UserInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import CategoryList from './components/CategoryList'
+import UserInput from './components/UserInput'
 
 function App() {
   return (
     <>
       <CategoryList/>
+      <UserInput/>
     </>
   )
 }
